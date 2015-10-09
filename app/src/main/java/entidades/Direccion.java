525e3197f4a6b2c82d58fa50efd5da45f1efe8bb
ttaattaann <s.perines@alumnos.duoc.cl>
2015-10-08 23:55:59 -0300

    creacion de 3 clases y sus constructores

--- app/src/main/java/entidades/Direccion.java
+++ app/src/main/java/entidades/Direccion.java
@@ -7,4 +7,79 @@ public class Direccion {
     private int idDireccion;
     private int nroCasa, nroCalle, areaLocal;
     private String ciudad, provincia, pais, otrosDetalles;
+
+    public Direccion(String otrosDetalles, String pais, String provincia, String ciudad, int areaLocal, int nroCalle, int nroCasa, int idDireccion) {
+        this.otrosDetalles = otrosDetalles;
+        this.pais = pais;
+        this.provincia = provincia;
+        this.ciudad = ciudad;
+        this.areaLocal = areaLocal;
+        this.nroCalle = nroCalle;
+        this.nroCasa = nroCasa;
+        this.idDireccion = idDireccion;
+    }
+
+    public int getIdDireccion() {
+        return idDireccion;
+    }
+
+    public void setIdDireccion(int idDireccion) {
+        this.idDireccion = idDireccion;
+    }
+
+    public int getNroCasa() {
+        return nroCasa;
+    }
+
+    public void setNroCasa(int nroCasa) {
+        this.nroCasa = nroCasa;
+    }
+
+    public int getNroCalle() {
+        return nroCalle;
+    }
+
+    public void setNroCalle(int nroCalle) {
+        this.nroCalle = nroCalle;
+    }
+
+    public int getAreaLocal() {
+        return areaLocal;
+    }
+
+    public void setAreaLocal(int areaLocal) {
+        this.areaLocal = areaLocal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getOtrosDetalles() {
+        return otrosDetalles;
+    }
+
+    public void setOtrosDetalles(String otrosDetalles) {
+        this.otrosDetalles = otrosDetalles;
+    }
 }
--- app/src/main/java/entidades/organizacion.java
+++ app/src/main/java/entidades/organizacion.java
@@ -7,4 +7,52 @@ public class organizacion {
     private int idOrganizacion;
     private String nombreO, descripcionO, otrosDetallesO;
     private int fonoO;
+
+    public organizacion(int idOrganizacion, String nombreO, String descripcionO, String otrosDetallesO, int fonoO) {
+        this.idOrganizacion = idOrganizacion;
+        this.nombreO = nombreO;
+        this.descripcionO = descripcionO;
+        this.otrosDetallesO = otrosDetallesO;
+        this.fonoO = fonoO;
+    }
+
+    public int getIdOrganizacion() {
+        return idOrganizacion;
+    }
+
+    public void setIdOrganizacion(int idOrganizacion) {
+        this.idOrganizacion = idOrganizacion;
+    }
+
+    public String getNombreO() {
+        return nombreO;
+    }
+
+    public void setNombreO(String nombreO) {
+        this.nombreO = nombreO;
+    }
+
+    public String getDescripcionO() {
+        return descripcionO;
+    }
+
+    public void setDescripcionO(String descripcionO) {
+        this.descripcionO = descripcionO;
+    }
+
+    public String getOtrosDetallesO() {
+        return otrosDetallesO;
+    }
+
+    public void setOtrosDetallesO(String otrosDetallesO) {
+        this.otrosDetallesO = otrosDetallesO;
+    }
+
+    public int getFonoO() {
+        return fonoO;
+    }
+
+    public void setFonoO(int fonoO) {
+        this.fonoO = fonoO;
+    }
 }
