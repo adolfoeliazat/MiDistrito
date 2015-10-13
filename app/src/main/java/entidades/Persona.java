4934ecf405fed75af8e026bac301dd304b71f7f3
Alejandro <alejandrosuns@gmail.com>
2015-10-13 00:44:26 -0300

    Sugerencia inicial clases.

--- app/src/main/java/entidades/Persona.java
+++ app/src/main/java/entidades/Persona.java
@@ -14,5 +14,15 @@ public class Persona {
     private Date fechaNacimiento;
     private String detalles;
 
-
+    public Persona(int rut, Date fechaInscripcion, String primerNombre, String segundoNombre, String apellidoPaterno, String apellidoMaterno, String genero, Date fechaNacimiento, String detalles) {
+        this.rut = rut;
+        this.fechaInscripcion = fechaInscripcion;
+        this.primerNombre = primerNombre;
+        this.segundoNombre = segundoNombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.genero = genero;
+        this.fechaNacimiento = fechaNacimiento;
+        this.detalles = detalles;
+    }
 }
