5c2c8c014b65144e5745de7c29c29eb20d25aa48
cecurin <ce.curin@alumnos.duoc.cl>
2015-10-22 22:00:33 -0300

    Diseñada la actividad Evento, con datos ficticios sosí

---
+++ app/src/main/java/com/midistrito/com/midistrito/FragmentoTelefonosFragment.java
@@ -0,0 +1,22 @@
+package com.midistrito.com.midistrito;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FragmentoTelefonosFragment extends Fragment {
+
+    public FragmentoTelefonosFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fragmento_telefonos, container, false);
+    }
+}
